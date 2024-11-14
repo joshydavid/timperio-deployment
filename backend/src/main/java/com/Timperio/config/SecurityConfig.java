8f53b7badc5d60a07f7b52cf5a5d8032b60edc24
joshydavid <cxang.2022@scis.smu.edu.sg>
2024-11-14 02:31:30 +0000

    refactor: code clean up, remove duplicate codes
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/Timperio/config/SecurityConfig.java
+++ backend/src/main/java/com/Timperio/config/SecurityConfig.java
@@ -44,9 +44,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/auth/login").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers(("/swagger-ui/**")).permitAll()
+                        .requestMatchers("/api/v1/auth/login", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/api/v1/purchaseHistory")
                         .hasAnyRole(Role.MARKETING.toString(), Role.SALES.toString())
                         .requestMatchers("/api/v1/export")
