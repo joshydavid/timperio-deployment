13b22e4d9f09d19c830879c6677e77c1b535047e
joshydavid <cxang.2022@scis.smu.edu.sg>
2024-11-16 11:24:10 +0000

    fix: remove unused code
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- backend/src/main/java/com/Timperio/service/NewsletterServiceImpl.java
+++ backend/src/main/java/com/Timperio/service/NewsletterServiceImpl.java
@@ -101,7 +101,6 @@ public class NewsletterServiceImpl implements NewsletterService {
 
         String requestBody = String.format("{\"html\":\"%s\"}", htmlContent.replace("\"", "\\\""));
 
-        System.out.println(requestBody);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBasicAuth("anystring", this.mailChimpConstant.getAPI_KEY());
@@ -124,8 +123,6 @@ public class NewsletterServiceImpl implements NewsletterService {
         String[] emails = customers.stream().map(Customer::getCustomerEmail).toArray(String[]::new);
 
         String[] additionalEmails = newsletterRequestDTO.getEmails().toArray(String[]::new);
-        System.out.println("emails: " + Arrays.toString(emails));
-        System.out.println(Arrays.toString(additionalEmails));
 
         String[] allEmails = Stream.concat(Arrays.stream(emails), Arrays.stream(additionalEmails))
                 .toArray(String[]::new);
@@ -149,16 +146,13 @@ public class NewsletterServiceImpl implements NewsletterService {
                 message.setSubject("test");
                 message.setContent(newsletterRequestDTO.getHtmlContent(), "text/html");
 
-                // Add up to 100 recipients to BCC
                 for (int j = i; j < i + 100 && j < allEmails.length; j++) {
                     message.addRecipient(Message.RecipientType.BCC, new InternetAddress(allEmails[j]));
                 }
 
                 Transport.send(message);
-                System.out.println("Batch sent successfully.");
 
-                // Optional: Introduce a delay between batches
-                Thread.sleep(1000); // 1 second delay
+                Thread.sleep(1000);
             }
         } catch (MessagingException | InterruptedException e) {
             e.printStackTrace();
@@ -167,104 +161,5 @@ public class NewsletterServiceImpl implements NewsletterService {
         return ResponseEntity.ok("Sent message successfully....");
 
     }
-
-    // public ResponseEntity<String> sendNewsletter(NewsletterRequestDTO
-    // newsletterRequestDTO) {
-    // NewsletterResponseDTO responseDTO = new
-    // NewsletterResponseDTO(newsletterRequestDTO.getEmail(), "success");
-    // String hashedEmail = this.md5Encode(newsletterRequestDTO.getEmail());
-    // CustomerSegment customerSegment = newsletterRequestDTO.getCustomerSegment();
-    // List<Customer> customers =
-    // this.customerService.getCustomerBySegment(customerSegment);
-    // String[] emails =
-    // customers.stream().map(Customer::getCustomerEmail).toArray(String[]::new);
-
-    // String[] additionalEmails =
-    // newsletterRequestDTO.getEmails().toArray(String[]::new);
-    // System.out.println("emails: " + Arrays.toString(emails));
-    // System.out.println(Arrays.toString(additionalEmails));
-
-    // String[] allEmails = Stream.concat(Arrays.stream(emails),
-    // Arrays.stream(additionalEmails))
-    // .toArray(String[]::new);
-    // String htmlContent = newsletterRequestDTO.getHtmlContent();
-
-    // // ============ Create Campaign ============
-    // System.out.println("=========== Create Campaign ===========");
-
-    // String datacenter = this.mailChimpConstant.getDatacenter();
-    // String campaignId = "a8be941ee5";
-    // String url = String.format("https://%s.api.mailchimp.com/3.0/campaigns",
-    // datacenter);
-
-    // HttpHeaders headers = new HttpHeaders();
-    // headers.setContentType(MediaType.APPLICATION_JSON);
-    // headers.setBasicAuth("anystring", this.mailChimpConstant.getAPI_KEY());
-
-    // String requestBody = "{\"type\":\"regular\"}";
-
-    // HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-    // ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST,
-    // entity, String.class);
-    // System.out.println(exchange.getBody());
-    // System.out.println("=========== End of Create Campaign ===========");
-
-    // // ============ Create Audience List ============
-    // System.out.println("=========== Create Audience List ===========");
-    // url = String.format("https://%s.api.mailchimp.com/3.0/lists", datacenter);
-    // requestBody = "{\"name\":\"Timperio Newsletter\",
-    // \"contact\":{\"company\":\"Timperio\",\"address1\":\"SMU\",\"city\":\"Singapore\",\"zip\":\"469060\",\"state\":\"Singapore\",\"country\":\"Singapore\"},
-    // \"permission_reminder\":\"You signed up for Timperio's newsletter\",
-    // \"campaign_defaults\":{\"from_name\":\"Timperio\",\"from_email\":\"hello@timperio.com\",\"subject\":\"Timperio
-    // Newsletter\",\"language\":\"en\"}, \"email_type_option\":false}";
-
-    // entity = new HttpEntity<>(requestBody, headers);
-    // exchange = restTemplate.exchange(url, HttpMethod.POST,
-    // entity, String.class);
-    // System.out.println(exchange.getBody());
-    // String audienceListId = new JSONObject(exchange.getBody()).getString("id");
-    // System.out.println("=========== End of Create Audience List ===========");
-
-    // // ============ Add Members to Audience List ============
-    // System.out.println("=========== Add Members to Audience List ===========");
-    // JSONArray members = new JSONArray();
-
-    // for (String email : allEmails) {
-    // if (email != null && !email.trim().isEmpty()) { // Check for non-null and
-    // non-empty emails
-    // JSONObject memberData = new JSONObject();
-    // memberData.put("email_address", email.trim());
-    // memberData.put("status", "subscribed");
-
-    // // Log the JSON request body for each member
-    // System.out.println("Request JSON for email: " + email + " - " +
-    // memberData.toString());
-
-    // url = String.format("https://%s.api.mailchimp.com/3.0/lists/%s/members",
-    // datacenter, audienceListId);
-    // entity = new HttpEntity<>(memberData.toString(), headers);
-
-    // try {
-    // exchange = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-    // System.out.println("Response for email: " + email + " - " +
-    // exchange.getBody());
-    // } catch (HttpClientErrorException e) {
-    // System.out.println("Error adding email: " + email + " - " +
-    // e.getResponseBodyAsString());
-    // }
-    // } else {
-    // System.out.println("Skipping invalid email: " + email);
-    // }
-    // }
-    // System.out.println("=========== End of Add Members to Audience List
-    // ===========");
-    // // if (exchange.getStatusCode().is2xxSuccessful()) {
-    // // return exchange;
-    // // } else {
-    // // return ResponseEntity.status(exchange.getStatusCode()).body(null);
-    // // }
-    // return exchange;
-
-    // }
 
 }
